6b663365b9150f1e72b9e4ad6b89ecb2f6ede91d
Arnaud <degardinarnaud@gmail.com>
2018-04-03 17:22:18 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" http://www.google.com 
-ping 127.1.1.1 (or pause or sleep ... it is just to cause a small delay)
+start firefox.exe http://www.google.com 
+ping 127.1.1.1 
 
 start /B "" "C:\Program Files\Mozilla Firefox\firefox.exe" imacros://run/?m=mm2.js
 
