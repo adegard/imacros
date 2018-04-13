336545461e3fe5fd87ab466616fe4914da564306
Arnaud <degardinarnaud@gmail.com>
2018-04-13 14:43:44 +0200

    Clicca Mi piace Instagram

---
+++ #Instan_MiPiace.js
@@ -0,0 +1,17 @@
+﻿
+var macro; 
+macro =  "CODE:";
+macro +=  "VERSION BUILD=9030808 RECORDER=FX" + "\n";
+macro +=  "TAB T=1" + "\n";
+
+for (i = 1; i <= 20; i++) {
+		for (j = 1; j <= 3; j++) {
+		macro +=  "TAG POS=" + j + " TYPE=SPAN ATTR=TXT:Mi<SP>piace" + "\n";
+		//macro +=  "WAIT SECONDS={{!VAR1}}" + "\n";
+		macro +=  "WAIT SECONDS=1" + "\n";
+		}
+macro +=  "WAIT SECONDS=1" + "\n";
+}
+
+iimPlay(macro)
+
