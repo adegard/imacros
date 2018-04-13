46f9abd6ae0f6d74ec20e37440ae4450df7ffed3
Arnaud <degardinarnaud@gmail.com>
2018-04-13 14:37:57 +0200

    Clicca su pulsanti segui di pagine Instagram

---
+++ #Instan_follow.js
@@ -0,0 +1,17 @@
+﻿
+var macro; 
+macro =  "CODE:";
+macro +=  "VERSION BUILD=9030808 RECORDER=FX" + "\n";
+macro +=  "TAB T=1" + "\n";
+
+
+for (j = 1; j <= 100; j++) {
+
+macro +=  "TAG POS=" + j + " TYPE=BUTTON ATTR=TXT:Segui" + "\n";
+//macro +=  "WAIT SECONDS=1" + "\n";
+
+}
+
+
+iimPlay(macro)
+
