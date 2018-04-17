6dc80d85336a8c7dc32ad7a75ae95d5d5529470c
Arnaud <degardinarnaud@gmail.com>
2018-04-17 16:27:28 +0200

    Macros click collegarsi LinkedIn js

---
+++ #IN_collegarsi_clicks.js
@@ -0,0 +1,21 @@
+﻿var macro, i, j; 
+var mydelay;
+ 
+macro =  "CODE:";
+macro +=  "VERSION BUILD=9030808 RECORDER=FX" + "\n";
+macro +=  "TAB T=1" + "\n";
+macro +=  "URL GOTO=https://www.linkedin.com/mynetwork/" + "\n";
+
+for (i = 0; i <= 10; i++) {
+	
+	for (j = 0; j <= 10; j++) {
+
+		macro +=  "TAG POS=1 TYPE=BUTTON ATTR=TXT:Collegati*" + "\n";
+		
+		mydelay=Math.round(3*Math.random());
+		iimDisplay("Random wait t="+mydelay)
+		macro +=  "WAIT SECONDS=" + mydelay  + "\n";
+	}
+	macro +=  "URL GOTO=https://www.linkedin.com/mynetwork/" + "\n";
+}
+iimPlay(macro)
