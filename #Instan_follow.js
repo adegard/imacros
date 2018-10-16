e0a448abe2c3a3a9696fb6489406cca7e2d95f07
Arnaud <degardinarnaud@gmail.com>
2018-10-16 11:48:16 +0200

    Update #Instan_follow.js

--- #Instan_follow.js
+++ #Instan_follow.js
@@ -7,7 +7,7 @@ macro +=  "TAB T=1" + "\n";
 
 for (j = 1; j <= 100; j++) {
 
-macro +=  "TAG POS=" + j + " TYPE=BUTTON ATTR=TXT:Segui" + "\n";
+macro +=  "TAG POS=" + j + " TYPE=BUTTON ATTR=TXT:Follow" + "\n";
 //macro +=  "WAIT SECONDS=1" + "\n";
 
 }
